996a617176c371d9793d4e91db524abc91bc5c08
Shravani <shravanikongaa@gmail.com>
2024-09-26 14:00:23 +0530

    Armstrong Number

---
+++ ArmstrongApp/Models/Armstrong.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmstrongApp.Models
+{
+    internal class Armstrong
+    {
+        public bool CheckArmstrong(int input)
+        {
+            int len = input.ToString().Length;
+            int num = input;
+            double result = 0;
+
+            if (num < 0)
+                return false;
+            else if (num == 0)
+                return true;
+            else
+            {
+                while (num > 0)
+                {
+                    int remainder = num % 10;
+                    result = result + Math.Pow(remainder, len);
+                    num = num / 10;
+                }
+            }
+
+            if (result == input)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ ArmstrongApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using ArmstrongApp.Models;
+
+namespace ArmstrongApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Armstrong armstrong = new Armstrong();
+            Console.WriteLine("Enter a number to check whether it is armstrong or not:");
+            int input=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            if (armstrong.CheckArmstrong(input))
+                Console.WriteLine($"{input} is an armstrong number");
+            else
+                Console.WriteLine($"{input} is not a armstrong number");
+            
+        }
+    }
+}
