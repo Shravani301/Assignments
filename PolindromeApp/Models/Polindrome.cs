eb780e04ccfda3b473db6d3bdf6e66c6b544e58e
Shravani <shravanikongaa@gmail.com>
2024-09-26 13:19:55 +0530

    Polindrome

---
+++ PolindromeApp/Models/Polindrome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolindromeApp.Models
+{
+    internal class Polindrome
+    {
+        public int ReverseGivenNumber(int input)
+        {
+            int result = 0;
+            int num = input;
+            if (num < 0)
+            {
+                return -1;
+            }
+            while (num > 0)
+            {
+                int remainder = num % 10;
+                num = num / 10;
+                result = result * 10 + remainder;
+            }
+            return result;
+        }
+
+        public bool CheckPolindrome(int input)
+        {
+            if (!((ReverseGivenNumber(input)) == input) || ReverseGivenNumber(input)==-1)
+                return false;
+            return true;
+        }
+    }
+}
---
+++ PolindromeApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using PolindromeApp.Models;
+
+namespace PolindromeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Polindrome polindrome = new Polindrome();
+            Console.WriteLine("Enter a Number to check whether it is polindrome or not:");
+            int inputNumber=Convert.ToInt32(Console.ReadLine());
+            if (polindrome.CheckPolindrome(inputNumber))
+                Console.WriteLine($"Given Number is Polindrome: {inputNumber}");
+            else
+                Console.WriteLine($"Given Number is Not a Polindrome: {inputNumber}");
+
+        }
+    }
+}
