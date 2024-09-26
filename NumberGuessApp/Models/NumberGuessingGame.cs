28475ee7faad676e27af7d8564a98ccda286ad0a
Shravani <shravanikongaa@gmail.com>
2024-09-26 12:25:56 +0530

    Number Guessing Game

---
+++ NumberGuessApp/Models/NumberGuessingGame.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessApp.Models
+{
+    internal class NumberGuessingGame
+    {
+        private int targetNumber { get; set; }
+        static int tries;
+        private Random random;
+        const int lowerBound = 1;
+        const int upperBound = 100;
+
+        public NumberGuessingGame()
+        {
+            random = new Random();
+            ResetGame();
+        }
+
+        public void ResetGame()
+        {
+            targetNumber = random.Next(lowerBound, upperBound);
+            tries = 0;
+        }
+
+        public int GetTries()
+        {
+            return tries;
+        }
+
+        public int GuessNumber(int guess)
+        {
+            tries++;
+            if (guess < targetNumber)
+            {
+                return -1; 
+            }
+            else if (guess > targetNumber)
+            {
+                return 1; 
+            }
+            return 0; 
+        }
+    }
+}
---
+++ NumberGuessApp/Program.cs
@@ -0,0 +1,65 @@
+﻿using NumberGuessApp.Models;
+
+namespace NumberGuessApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberGuessingGame game = new NumberGuessingGame();
+
+            Console.WriteLine("Welcome to Guess Number Game");
+
+            string playAgain;
+            do
+            {
+                PlayGame(game);
+                Console.Write("Do you want to play again? (y/n): ");
+                playAgain = Console.ReadLine() ?? string.Empty;
+
+                if (playAgain.ToLower() == "y")
+                {
+                    game.ResetGame();
+                }
+
+            } while (playAgain.ToLower() == "y");
+
+            Console.WriteLine("Thanks for playing!");
+        }
+
+        private static void PlayGame(NumberGuessingGame game)
+        {
+            Console.WriteLine("You Will Be Asked To Guess A Number To Win The Game");
+            int guess;
+            int result;
+
+            do
+            {
+                Console.Write("Enter a guess number between 1 to 100: ");
+                if (int.TryParse(Console.ReadLine(), out guess))
+                {
+                    result = game.GuessNumber(guess);
+
+                    if (result == -1)
+                    {
+                        Console.WriteLine("Oops! Sorry, too low.");
+                    }
+                    else if (result == 1)
+                    {
+                        Console.WriteLine("Oops! Sorry, too high.");
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine($"Yayyy! Your guessed number is correct. You took {game.GetTries()} tries!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                    result = -1;
+                }
+            } while (result != 0);
+        }
+    }
+}
