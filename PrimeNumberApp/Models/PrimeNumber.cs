0e395b4e0bea76a583cf3bb56dffbd84d5b12f28
Shravani <shravanikongaa@gmail.com>
2024-09-26 12:21:10 +0530

    Add project files.

---
+++ PrimeNumberApp/Models/PrimeNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberApp.Models
+{
+    internal class PrimeNumber
+    {
+        int number { get; set; }
+
+        public bool IsPrimeOrNot(int input)
+        {
+            if (input < 2)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(input); i++)
+            {
+                if (input % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ PrimeNumberApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using PrimeNumberApp.Models;
+
+namespace PrimeNumberApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrimeNumber primeNumber = new PrimeNumber();
+
+            Console.WriteLine("Enter a number to check whether it is prime or not:");
+
+            int myNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (primeNumber.IsPrimeOrNot(myNumber))
+                Console.WriteLine("Given Number is Prime:" + myNumber);
+            else
+                Console.WriteLine("Given Number is not an Prime:" + myNumber);
+        }
+    }
+}
