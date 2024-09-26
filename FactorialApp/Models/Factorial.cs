adc4ee4c46154bab902efe833b785b872af244af
Shravani <shravanikongaa@gmail.com>
2024-09-26 14:13:40 +0530

    Factorial

---
+++ FactorialApp/Models/Factorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialApp.Models
+{
+    internal class Factorial
+    {
+        public double FindFactorial(int input)
+        {
+            double result=1;
+            if(input==0||input==1)
+                return 1;
+            for (int i = 1; i <= input; i++)
+            {
+                result=result*i;
+            }
+            return result;
+        }
+    }
+}
---
+++ FactorialApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using FactorialApp.Models;
+
+namespace FactorialApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Factorial factorial = new Factorial();
+            Console.WriteLine("Enter a Number to find out factorial:");
+            int input=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Factorial of {input} is:{factorial.FindFactorial(input)}");
+
+        }
+    }
+}
