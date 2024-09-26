229925085e0210f9d481458122959cc54431e0e8
Shravani <shravanikongaa@gmail.com>
2024-09-26 12:45:59 +0530

    ReverseNumber

---
+++ ReverseNumberApp/Models/ReverseNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumberApp.Models
+{
+    internal class ReverseNumber
+    {
+        public int ReverseGivenNumber(int input)
+        { 
+            int result = 0;
+            int num = input;
+            while (num > 0)
+            {
+                int remainder = num % 10;
+                num = num / 10;
+                result = result * 10 + remainder;
+            }
+            return result;
+        }
+    }
+}
---
+++ ReverseNumberApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using ReverseNumberApp.Models;
+
+namespace ReverseNumberApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseNumber reverseNumber = new ReverseNumber();
+            Console.WriteLine("Enter a Number for Reverse:");
+            int myNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Reverse of a {myNumber}  number is: "+reverseNumber.ReverseGivenNumber(myNumber));
+        }
+    }
+}
