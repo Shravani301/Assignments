a6df11cd04f32e7ea016e6ddf4226add1e86545e
Shravani <shravanikongaa@gmail.com>
2024-10-23 09:23:37 +0530

    state design pattern

---
+++ StateDesignPattern/ATMMachine.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateDesignPattern
+{
+    //State Design Pattern allows an object to change its behavior depending
+    //on its current internal state
+    internal class ATMMachine : IATMState
+    {
+       public IATMState AtmMachineState;
+        public ATMMachine()
+        {
+            AtmMachineState = new DebitCardNotInsertedState();
+        }
+        public void InsertDebitCard()
+        {
+            AtmMachineState.InsertDebitCard();
+
+            if (AtmMachineState is DebitCardNotInsertedState)
+            {
+                AtmMachineState = new DebitCardInsertedState();
+                Console.WriteLine($"ATM Machine internal state has been changed to : {AtmMachineState.GetType().Name}");
+            }
+        }
+        public void EjectDebitCard()
+        {
+            AtmMachineState.EjectDebitCard();
+
+            if (AtmMachineState is DebitCardInsertedState)
+            {
+                AtmMachineState = new DebitCardNotInsertedState();
+                Console.WriteLine($"ATM Machine internal state has been Changed to : {AtmMachineState.GetType().Name}");
+            }
+        }
+        public void EnterPin()
+        {
+            AtmMachineState.EnterPin();
+        }
+        public void WithdrawMoney()
+        {
+            AtmMachineState.WithdrawMoney();
+        }
+    }
+}
---
+++ StateDesignPattern/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace StateDesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ATMMachine atmMachine = new ATMMachine();
+            Console.WriteLine("ATM Machine Current state : "
+                            + atmMachine.AtmMachineState.GetType().Name);
+            Console.WriteLine();
+            atmMachine.EnterPin();
+            atmMachine.WithdrawMoney();
+            atmMachine.EjectDebitCard();
+            atmMachine.InsertDebitCard();
+            Console.WriteLine();
+            Console.WriteLine("ATM Machine Current state : "
+                            + atmMachine.AtmMachineState.GetType().Name);
+            Console.WriteLine();
+            atmMachine.EnterPin();
+            atmMachine.WithdrawMoney();
+            atmMachine.InsertDebitCard();
+            atmMachine.EjectDebitCard();
+            Console.WriteLine("");
+            Console.WriteLine("ATM Machine Current state : "
+                            + atmMachine.AtmMachineState.GetType().Name);
+            
+        }
+        
+    }
+}
